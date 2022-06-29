019b9704717d2776e0a231b6bc607226962baa82
nirg122 <nirg122@gmail.com>
2022-06-29 18:26:11 +0300

    Added API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '238f37fc-3432-4d22-8512-f7bc7d48dfe9';
 export const MODAL_CLOSE_SEC = 2.5;
